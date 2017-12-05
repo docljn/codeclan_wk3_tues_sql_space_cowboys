6b343682472a7dad7e799084e93dbc87224aab15
Graeme Brown <graemehbrown@googlemail.com>
2017-12-05 16:13:57 +0000

    Console.rb file created.

---
+++ console.rb
@@ -0,0 +1,24 @@
+require ("pry-byebug")
+require_relative("./model/bounty.rb")
+
+bounty1 = Bounty.new({
+  'name' => 'Red',
+  'bounty_value' => '250',
+  'favourite_weapon' => 'ray gun',
+  'collected_by' => 'Dread Pirate Roberts'
+  })
+
+bounty2 = Bounty.new({
+  'name' => 'Purple',
+  'bounty_value' => '150',
+  'favourite_weapon' => 'phazer',
+  'collected_by' => 'Princess Leia'
+  })
+
+  bounty1.save()
+  bounty2.save()
+
+
+  binding.pry
+  nil
+  
