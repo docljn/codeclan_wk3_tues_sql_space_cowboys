9f47ede9a4a0f9382696d3fd583962b120a648d2
DocLJN <ljnoble@gmail.com>
2017-12-05 20:02:12 +0000

    Fixed indentation.

--- model/bounty.rb
+++ model/bounty.rb
@@ -12,58 +12,58 @@ class Bounty
     @collected_by = options['collected_by']
   end
 
-def save()
-  db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
-  sql = "
-      INSERT INTO bounties (
-        name,
-        bounty_value,
-        favourite_weapon,
-        collected_by
-      ) VALUES (
-        $1, $2, $3, $4
-      ) RETURNING *
-  ;"
-  values = [@name, @bounty_value, @favourite_weapon, @collected_by]
-  db.prepare("save", sql)
-  @id = db.exec_prepared("save", values)[0]['id'].to_i
-  db.close
-end
-
-def update()
-  db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
-  sql = "
-      UPDATE bounties SET (
+  def save()
+    db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
+    sql = "
+        INSERT INTO bounties (
           name,
           bounty_value,
           favourite_weapon,
           collected_by
-        ) = (
+        ) VALUES (
           $1, $2, $3, $4
-        ) WHERE id = $5
-          ;"
-  values = [@name, @bounty_value, @favourite_weapon, @collected_by, @id]
-  db.prepare("update", sql)
-  db.exec_prepared("update", values)
-  db.close
-end
+        ) RETURNING *
+    ;"
+    values = [@name, @bounty_value, @favourite_weapon, @collected_by]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]['id'].to_i
+    db.close
+  end
 
-def delete()
-      db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
-      sql = "DELETE FROM bounties WHERE id = $1;"
-      values = [@id]
-      db.prepare("delete_one", sql)
-      db.exec_prepared("delete_one", values)
-      db.close()
+  def update()
+    db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
+    sql = "
+        UPDATE bounties SET (
+            name,
+            bounty_value,
+            favourite_weapon,
+            collected_by
+          ) = (
+            $1, $2, $3, $4
+          ) WHERE id = $5
+            ;"
+    values = [@name, @bounty_value, @favourite_weapon, @collected_by, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close
+  end
+
+  def delete()
+    db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
+    sql = "DELETE FROM bounties WHERE id = $1;"
+    values = [@id]
+    db.prepare("delete_one", sql)
+    db.exec_prepared("delete_one", values)
+    db.close()
   end
 
   def self.delete_all()
-        db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
-        sql = "DELETE FROM bounties;"
-        db.prepare("delete_all", sql)
-        db.exec_prepared("delete_all")
-        db.close()
-    end
+    db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
+    sql = "DELETE FROM bounties;"
+    db.prepare("delete_all", sql)
+    db.exec_prepared("delete_all")
+    db.close()
+  end
 
   def find_by_id()
     db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
