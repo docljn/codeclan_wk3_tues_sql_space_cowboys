3e28ec6e0a9887115833e4554cae3dc6cb19b9e3
Graeme Brown <graemehbrown@googlemail.com>
2017-12-05 16:06:27 +0000

    Save method.

--- model/bounty.rb
+++ model/bounty.rb
@@ -10,4 +10,28 @@ class Bounty
     @collected_by = options['collected_by']
   end
 
+#save
+
+def save()
+  db = PG.connect({ dbname: 'space_cowboys', host: 'localhost'})
+  sql = "
+      INSERT INTO bounties (
+        name,
+        bounty_value,
+        favourite_weapon,
+        collected_by
+      ) VALUES (
+        $1, $2, $3, $4
+      ) RETURNING *
+  ;"
+  values = [@name, @bounty_value, @favourite_weapon, @collected_by]
+  db.prepare("save", sql)
+  @id = db.exec_prepared("save", values)[0]['id'].to_i
+  db.close
+end
+
+#update
+
+#delete
+
 end
