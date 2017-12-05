42ba581ebbe902a9d285f6ab7899c3403fdd3b1a
Graeme Brown <graemehbrown@googlemail.com>
2017-12-05 16:20:18 +0000

    Added require('pg') to Bounty class.

--- model/bounty.rb
+++ model/bounty.rb
@@ -1,8 +1,10 @@
+require('pg')
+
 class Bounty
   attr_reader :id
   attr_accessor :name, :bounty_value, :favourite_weapon, :collected_by
 
-  def intitialize(options)
+  def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
     @bounty_value = options['bounty_value'].to_i
